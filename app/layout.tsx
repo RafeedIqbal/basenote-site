b834d6c855aa6c9ed7eb22c11e7e7112acce6696
rafeed-iconfitness <rafeediqbal@gmail.com>
2026-07-28 22:20:48 +0600

    Add Google Tag Manager analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Lexend_Deca, Source_Serif_4, Space_Mono } from "next/font/google";
+import Script from "next/script";
 
 import SmoothScroll from "@/components/SmoothScroll";
 import { SITE_DESCRIPTION, SITE_NAME, SITE_ORIGIN } from "@/lib/site";
@@ -74,7 +75,25 @@ export default function RootLayout({
       lang="en"
       className={`${sourceSerif.variable} ${lexendDeca.variable} ${spaceMono.variable}`}
     >
+      <head>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NWTC77JT');`}
+        </Script>
+      </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NWTC77JT"
+            height="0"
+            width="0"
+            title="Google Tag Manager"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <a href="#main-content" className="skipLink">
           Skip to content
         </a>
